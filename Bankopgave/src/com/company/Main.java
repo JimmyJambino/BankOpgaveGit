204a52142447719be8c8ec0376205902516477df
Raskoe99 <r.koefoed@hotmail.com>
2020-10-27 15:23:05 +0100

    Skrevet i main

--- Bankopgave/src/com/company/Main.java
+++ Bankopgave/src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    public String inputString() {
+        Scanner in = new Scanner(System.in);
+        String brugerinput = in.nextLine();
+        return brugerinput;
+    }
+
+    public static int inputInt() {
+        Scanner in = new Scanner(System.in);
+        while (!in.hasNextInt()) {
+            int temp;
+            while ()
+            System.out.println("Indsæt venligst et helt tal: \n");
+            in.nextInt();
+
+        }
+        int brugerinput = in.nextInt();
+        return brugerinput;
+    }
+
+    public String inputDouble() {
+        Scanner in = new Scanner(System.in);
+
+        String brugerinput = in.nextLine();
+        return brugerinput;
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(inputInt());
     }
 }
