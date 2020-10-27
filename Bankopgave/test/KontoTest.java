c2d2b9aeb40a55aac943c87b983ce986d1e4c2ee
JimmyJambino <jimmy_losang@hotmail.com>
2020-10-27 15:41:33 +0100

    test folder added

---
+++ Bankopgave/test/KontoTest.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class KontoTest {
+
+    @org.junit.jupiter.api.Test
+    void indsæt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hæv() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void overfør() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getKontonummer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setKontonummer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEjernavn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEjernavn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSaldo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSaldo() {
+    }
+}
